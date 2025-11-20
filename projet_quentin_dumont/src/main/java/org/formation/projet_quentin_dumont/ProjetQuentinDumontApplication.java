1dde143f396755aa7138688083140fdc170c0332
Quentin Dumont <quentin.dumont@epita.fr>
2025-11-20 14:28:15 +0100

    init project

---
+++ projet_quentin_dumont/src/main/java/org/formation/projet_quentin_dumont/ProjetQuentinDumontApplication.java
@@ -0,0 +1,13 @@
+package org.formation.projet_quentin_dumont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetQuentinDumontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetQuentinDumontApplication.class, args);
+    }
+
+}
---
+++ projet_quentin_dumont/src/test/java/org/formation/projet_quentin_dumont/ProjetQuentinDumontApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.projet_quentin_dumont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetQuentinDumontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
